83b7342fab78cebd7170c5a793bca8742baa4be1
theoKV <62474884+theoKV@users.noreply.github.com>
2021-11-03 09:20:50 +0100

    Add files via upload

---
+++ JS/Test.js
@@ -0,0 +1,33 @@
+console.log("Your MUM!?")
+/*Cumment*/ 
+/*const Bro = 2
+let moment = 3
+console.log("Nomber wahn: "+Bro + moment)
+let i = 0
+while (i<10){
+    console.log(i)
+    i++
+}
+
+let Buddy = ["joe mom","joe dad","Bruh"] 
+
+
+
+for (i = 0; i < Buddy.length; i++) {
+console.log(Buddy[i])
+}//bruuuuuuuuuuh, why you log all my buddies
+*/
+/*
+const input = prompt("What's your name?");
+alert(`Your name is ${input}`); lager en variabel som bruker bestemmer innhold. NB: FUNKER IKKE MED NODE JS*/
+
+var secondsToAdd=30;
+var currentDate = new Date();
+console.log(currentDate.getTime())
+
+var otherDate = new Date()
+    console.log(otherDate.getTime())
+
+let timeSpent = otherDate.getTime()-currentDate.getTime()
+console.log(timeSpent)
+
