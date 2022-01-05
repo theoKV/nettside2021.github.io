f55df1c92d13b7283e41a68f33ba80ab1a9f5e65
theoKV <62474884+theoKV@users.noreply.github.com>
2022-01-05 11:19:53 +0100

    Add files via upload

--- JS/Main.js
+++ JS/Main.js
@@ -26,3 +26,45 @@ function navBar() {
       x.style.display = "block";
     }
   }
+  var large = false
+  function Bilde(id){
+    /*CSS som brukes   
+    img{
+    transition-duration:0.4s ;
+    width: auto;
+    height:50%;
+    float: right;
+    
+  /* for runde hjørner på bildet border-radius: 100px;*/
+  /*Bilde endrer seg når pekeren er over den*/
+  /*
+  img:hover{
+    
+    height: 79%;
+    width: auto;
+    position:absolute;
+    border: 0px solid red;
+right:0%;
+top:1%;
+  }*/
+    /*Definerer hvliket bilde som skal brukes*/ 
+    var pic = document.getElementById(id).style;
+    /*hvis bildet er lite gjør det stort hvis ikke gjør det lite */
+    if(large == false){
+      pic.width = "auto"
+      pic.position = "absolute"
+      pic.right = "0%"
+      pic.top = "1%"
+      pic.height = "79%";
+      pic.transitionDuration = "0.4s"
+      large = true
+      
+    }else{
+      pic.transitionDuration = "0s"
+      pic.width = "auto"
+      pic.height = "50%"
+      large = false
+      pic.position = "static"
+    }
+    
+  }
